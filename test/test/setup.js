31565f280dbca603aac31f71310b6fc6d14f6f02
özgenur bilican <153661754+ozgenur19@users.noreply.github.com>
2024-08-09 21:29:33 +0300

    Update setup.js

--- test/test/setup.js
+++ test/test/setup.js
@@ -1,1 +1,19 @@
 
+// MongoDB bağlantısı için gerekli modülleri yükleyin
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+let mongoServer;
+
+// Testlerin başlamasından önce MongoDB'yi başlatın
+before(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+});
+
+// Testlerden sonra MongoDB'yi kapatın
+after(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
